d04d32468f3ecee87851f499a6ffb4d2ac5ff773
Mariah <mariahschock@gmail.com>
2022-07-11 09:05:11 -0700

    netlifly issues

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,12 +1,12 @@
 export async function getPokemon(filter) {
-  const rawData = await fetch(`http://.netlify/functions/pokemon?pokeQuery=${filter}`);
+  const rawData = await fetch(`https://.netlify/functions/pokemon?pokeQuery=${filter}`);
   const data = await rawData.json();
 
   return data;
 }
 
 export async function getYelp(filter) {
-  const rawData = await fetch(`http://.netlify/functions/yelp?yelpQuery=${filter}`);
+  const rawData = await fetch(`https://.netlify/functions/yelp?yelpQuery=${filter}`);
   const data = await rawData.json();
 
   return data;
